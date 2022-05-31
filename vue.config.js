7d4056d01ef0dd7316afc544ce81b41d8b6f1c54
vlamboy <vivienne.lamboy@student.htw-berlin.de>
2022-05-31 08:42:25 +0200

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
-const { defineConfig } = require('@vue/cli-service');
+// const { defineConfig } = require('@vue/cli-service');
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+// module.exports = defineConfig({
+//  transpileDependencies: true,
+module.exports = {
+  devServer: {
+    port: '3000',
+  },
+};
