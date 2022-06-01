f851cc1744153c8cf4cf41ec7ae8ce39d688f7fb
vlamboy <vivienne.lamboy@student.htw-berlin.de>
2022-06-01 09:40:11 +0200

    created Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>Das ist eine Navbar</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Navbar',
+};
+</script>
+
+<style scoped>
+
+</style>
