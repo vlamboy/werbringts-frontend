579ab0996e224787f8a0fd4826082d646af0ac04
vlamboy <vivienne.lamboy@student.htw-berlin.de>
2022-06-14 17:39:36 +0200

    Name Popup

--- src/components/NamePopup.vue
+++ src/components/NamePopup.vue
@@ -2,7 +2,7 @@
   <!-- Button trigger modal -->
   <button type="button" class="btn btn-primary" data-bs-toggle="modal"
           data-bs-target="#exampleModal">
-    Launch demo modal
+    Füge etwas zu deiner Liste hinzu
   </button>
 
   <!-- Modal -->
@@ -11,16 +11,18 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Gib hier deinen Namen ein</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
           </button>
         </div>
         <div class="modal-body">
-          ...
+          <label for="list-name" class="form-label">
+            <input type="text" class="form-control" id="list-name" v-model="firstname" />
+          </label>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Schließen</button>
+          <button type="button" class="btn btn-primary">Speichern</button>
         </div>
       </div>
     </div>
