f7efa71517477aa6105d593433d6fed8935ae9ca
vlamboy <vivienne.lamboy@student.htw-berlin.de>
2022-07-04 21:31:36 +0200

    updated AboutUsView.vue

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,5 +1,19 @@
 <template>
+  <br>
   <div class="aboutUs">
     <h1>About Us</h1>
+     <p>
+       <br>
+       Fünf mal hausgemachte Guacamole und viel zu wenig Kaltgetränke - der Streit über die absolut
+       wahrhaftige Zubereitung von Guac <br> und die Frustration über die fehlende Flüssignahrung
+       sind DER Partykiller.
+       Dieses tiefverankerte gesellschaftliche Problem <br>wollen wir mit „werbringts“ im
+       Keim ersticken.
+       <br>
+       <br>
+       werbringts unterstützt euch bei der Koordination der nächsten Party und hält euch
+       auf dem Laufenden,<br> was andere Gäste mitbringen,
+       sodass besagte Guac-Fiaskos der Vergangenheit angehören.
+     </p>
   </div>
 </template>
